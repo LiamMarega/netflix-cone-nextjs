6ec67ee736d754d62367f836f2760bf68386c479
Liam Marega <86364396+LiamMarega@users.noreply.github.com>
2024-02-09 11:36:05 -0300

    Agrega el paquete Nodemailer y configura proveedor de correo electrónico

--- src/app/utils/auth.ts
+++ src/app/utils/auth.ts
@@ -1,9 +1,10 @@
 import { NextAuthOptions } from 'next-auth';
 import prisma from './db';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { Adapter } from 'next-auth/adapters';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import { Adapter } from 'next-auth/adapters';
+import EmailProvider from 'next-auth/providers/email';
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -15,6 +16,17 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
     }),
   ],
 } satisfies NextAuthOptions;
